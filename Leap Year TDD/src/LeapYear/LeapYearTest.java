ff7ad7038ce65ef179adf15dff39a33dfd91fc5a
deepa2695 <deepamano2695@gmail.com>
2021-09-17 05:51:19 -0700

    Refactored the test class again to make the code simple

--- Leap Year TDD/src/LeapYear/LeapYearTest.java
+++ Leap Year TDD/src/LeapYear/LeapYearTest.java
@@ -1,15 +1,21 @@
 package LeapYear;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class LeapYearTest {
 	
-	public LeapYear leapYear;
+public LeapYear leapYear;
+	
+	@Before
+	public void initiaLizeLeapYearClass(){
+		leapYear = new LeapYear();
+	}
 	
 	@Test
     public void checkToGetOutput(){
-    	leapYear = new LeapYear();
         Assert.assertTrue(leapYear.isLeapYear(2000));
     }
+	
 }
